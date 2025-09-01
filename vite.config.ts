531a08dcd189e218f905b1d5cac9696890e2d8f2
Tiberiu58 <tiberiumatei1978@gmail.com>
2025-09-01 10:15:39 +0300

    Fix lodash.debounce import error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['lodash.debounce'],
     exclude: ['lucide-react', '@splinetool/react-spline'],
   },
 });
